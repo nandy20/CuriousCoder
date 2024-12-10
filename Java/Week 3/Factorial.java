40b6ac3d899fa625b82fea174dec166f15b30e7e
Nandhini.R <32575168+nandy20@users.noreply.github.com>
2024-12-11 00:06:56 +0530

    Create Factorial.java

---
+++ Java/Week 3/Factorial.java
@@ -0,0 +1,9 @@
+class Factorial {
+    public static void main(String[] args) {
+        int fact=1, n =5 ;
+        for(int i=1;i<=n;i++){
+          fact = fact * i;
+        }
+        System.out.println(fact);
+    }
+}
