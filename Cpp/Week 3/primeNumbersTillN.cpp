7b375387f9ca6037024e94b3b269807244b940a4
Nandhini.R <32575168+nandy20@users.noreply.github.com>
2024-12-11 00:37:11 +0530

    Create primeNumbersTillN.cpp

---
+++ Cpp/Week 3/primeNumbersTillN.cpp
@@ -0,0 +1,20 @@
+// Prints all the prime numbers from 1 to till n
+#include <iostream>
+using namespace std;
+int main() {
+    int n=9;
+    for(int x=1;x<=n;x++){
+        int flag = 0;
+        for(int i=2;i<x;i++){
+            if(x%i==0){
+                flag = 1;
+            }
+        }
+        if(flag==0){
+            cout<<x << endl;
+        }
+    }
+    
+    
+    return 0;
+}
