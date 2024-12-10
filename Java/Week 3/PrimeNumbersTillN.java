d4aa0451c8384690f5f845caf505aa5597eab0e7
Nandhini.R <32575168+nandy20@users.noreply.github.com>
2024-12-11 00:38:17 +0530

    Create PrimeNumbersTillN.java

---
+++ Java/Week 3/PrimeNumbersTillN.java
@@ -0,0 +1,17 @@
+
+class PrimeNumbersTillN {
+    public static void main(String[] args) {
+       int n=9;
+    for(int x=1;x<=n;x++){
+        int flag = 0;
+        for(int i=2;i<x;i++){
+            if(x%i==0){
+                flag = 1;
+            }
+        }
+        if(flag==0){
+            System.out.println(x);
+        }
+    }
+    }
+}
