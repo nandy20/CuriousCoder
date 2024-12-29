053b5cecc0e91a63079cfbe7c71ef66f538123d9
Nandhini.R <32575168+nandy20@users.noreply.github.com>
2024-12-30 01:17:43 +0530

    Create Recursion.java

---
+++ Java/Week 6/Recursion.java
@@ -0,0 +1,20 @@
+class Recursion {
+
+  // Count the number of zeros in a number recursively
+    static int countzero(int n){
+        if(n%10==n){
+            if(n%10==0){
+                return 1;
+            } 
+            return 0;
+         }
+        if(n%10==0){
+            return 1+countzero(n/10);
+        } else {
+            return countzero(n/10);
+        } 
+    }
+    public static void main(String[] args) {
+        System.out.println(countzero(300403));
+    }
+}
