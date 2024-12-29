f0496f5b51fe0287a24fe9106918518db46bb0d6
Nandhini.R <32575168+nandy20@users.noreply.github.com>
2024-12-30 01:20:18 +0530

    Create SumOfDigits.java

---
+++ Java/Week 6/Recursion/SumOfDigits.java
@@ -0,0 +1,14 @@
+
+class SumOfDigits {
+// Sum of digit of a number recursively
+static int sumOfDigits(int n){
+    if(n==0){
+        return 0;
+    }
+    return (n%10) + sumOfDigits(n/10);
+}
+
+    public static void main(String[] args) {
+        System.out.println(sumOfDigits(300403));
+    }
+}
