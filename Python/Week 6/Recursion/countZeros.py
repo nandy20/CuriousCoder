1d41cd46d944154c56773aa25d34f4178f1fbe4e
Nandhini.R <32575168+nandy20@users.noreply.github.com>
2024-12-30 01:50:00 +0530

    Create countZeros.py

---
+++ Python/Week 6/Recursion/countZeros.py
@@ -0,0 +1,12 @@
+def countzero(n):
+    if(n%10==n):
+       if(n%10==0):
+           return 1
+       else:
+           return 0
+    if(n%10==0):
+        return 1+countzero(n//10)
+    else:
+        return countzero(n//10)
+
+print(countzero(304050))
