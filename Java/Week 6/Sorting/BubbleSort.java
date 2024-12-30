4badad3645fcbff4e25dad5b5080150fd1a98585
Nandhini.R <32575168+nandy20@users.noreply.github.com>
2024-12-31 00:10:34 +0530

    Create BubbleSort.java

---
+++ Java/Week 6/Sorting/BubbleSort.java
@@ -0,0 +1,19 @@
+
+class BubbleSort {
+    public static void main(String[] args) {
+        int [] a = {2,35,45,345,324,23};
+        int n = 6;
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-1;j++){
+                if(a[j]>a[j+1]){
+                    int temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            System.out.println(a[i]);
+        }
+    }
+}
