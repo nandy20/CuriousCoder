cfcef2cc1d4531cdb8583a9194c50bdb8f1f826f
Nandhini.R <32575168+nandy20@users.noreply.github.com>
2025-01-14 22:08:07 +0530

    Create missingNumber.cpp

---
+++ Cpp/Week 8/missingNumber.cpp
@@ -0,0 +1,19 @@
+int missingNumber(vector<int>& a) {
+        // code here
+        int n = a.size();
+        sort(a.begin(),a.end());
+        int i=0,j=n-1;
+        while(i<j){
+            int mid=(i+j)/2;
+            if(a[mid]==mid+1){
+                i=mid+1;
+            }
+            else{
+                j=mid;
+            }
+        }
+        if(j+1 == a[n-1]){
+            return n+1;
+        }
+        return j+1;
+}
