cc4d23bd0c09bae9f921474bb6381b18ae01adbc
Nandhini.R <32575168+nandy20@users.noreply.github.com>
2025-01-26 23:37:25 +0530

    Create MiddleLinkedList.java

---
+++ Java/Linked List/MiddleLinkedList.java
@@ -0,0 +1,13 @@
+int getMiddle(Node head) {
+        Node temp, temp1;
+        temp=head;
+        temp1=head.next;
+        while(temp1!=null && temp1.next!=null){
+            temp=temp.next;
+            temp1=temp1.next.next;
+        }
+        if(temp1==null){
+            return temp.data;
+        }
+        return temp.next.data;
+    }
