7a4f53b00b206f606300cd66d247dcee9f0886a3
Nandhini.R <32575168+nandy20@users.noreply.github.com>
2025-03-24 14:26:50 +0530

    Create KthRotationSortedRotated.cpp

---
+++ Cpp/Binary search/KthRotationSortedRotated.cpp
@@ -0,0 +1,21 @@
+int findKRotation(vector<int> &a) {
+        // Code Here
+        int n=a.size();
+        int l=0, h=n-1, ans = 0;
+        while(l<=h){
+            int mid = l+((h-l)/2);
+            int i = (mid-1+n)%n;
+            int j = (mid+1)%n;
+            if(a[mid]<a[i] && a[mid]<a[j]){
+                return mid;
+            }
+            if(a[h]>a[mid]){
+                h=mid-1;
+            }else{
+                ans = mid;
+                l=mid+1;
+            }
+        }
+        return ans;
+        
+    }
