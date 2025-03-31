e7bff14455b1ae0c6976dbc7f307bc36f29b24ed
Nandhini.R <32575168+nandy20@users.noreply.github.com>
2025-03-31 20:43:41 +0530

    Create squaresDiff.cpp

---
+++ Cpp/Math/squaresDiff.cpp
@@ -0,0 +1,8 @@
+//https://www.geeksforgeeks.org/problems/squares-difference0939/1
+class Solution {
+  public:
+    long long int squaresDiff(int N){
+        // code here
+        return abs((N*(N+1)*(2*N+1))/6 - pow(N*(N+1)/2, 2));
+    }
+};
