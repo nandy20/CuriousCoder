69d64152588ca5294d6bdff423e5cdc605bbd771
Nandhini.R <32575168+nandy20@users.noreply.github.com>
2025-03-31 20:07:36 +0530

    Create Move all negative elements to end.cpp

---
+++ Cpp/Array/Move all negative elements to end.cpp
@@ -0,0 +1,22 @@
+//https://www.geeksforgeeks.org/problems/move-all-negative-elements-to-end1813/1
+ void segregateElements(vector<int>& arr) {
+        // Your code goes here
+        vector<int> pos , neg;
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]<0){
+                neg.push_back(arr[i]);
+            }
+             else{
+                 pos.push_back(arr[i]);
+             }
+        }
+        int j=0;
+        for(int i=0;i<pos.size();i++){
+            arr[j] = pos[i];
+            j++;
+        }
+        for(int i=0;i<neg.size();i++){
+            arr[j] = neg[i];
+            j++;
+        }
+    }
