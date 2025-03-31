da2cc26813e0f6f0510cc9fc560abda762d199ce
Nandhini.R <32575168+nandy20@users.noreply.github.com>
2025-03-31 20:44:36 +0530

    Create reverseDigits.cpp

---
+++ Cpp/Math/reverseDigits.cpp
@@ -0,0 +1,15 @@
+//https://www.geeksforgeeks.org/problems/reverse-digit0316/1
+class Solution {
+  public:
+    int reverseDigits(int n) {
+        // Code here
+        int x=0;
+        while(n>0){
+            if(n%10>0){
+                x=x*10+(n%10);
+            }
+            n=n/10;
+        }
+        return x;
+    }
+};
