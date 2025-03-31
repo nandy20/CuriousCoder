07cd24587869b78ed57978792e3137d051a9e317
Nandhini.R <32575168+nandy20@users.noreply.github.com>
2025-03-31 20:46:18 +0530

    Create printArray.cpp

---
+++ Cpp/Math/printArray.cpp
@@ -0,0 +1,7 @@
+//https://www.geeksforgeeks.org/problems/print-elements-of-array4910/1
+ void printArray(vector<int> &arr) {
+        // code here
+        for(int i=0;i<arr.size();i++){
+            cout<<arr[i]<<" ";
+        }
+    }
