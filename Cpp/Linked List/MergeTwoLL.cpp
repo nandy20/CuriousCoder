7edfc4ef2213558691369a97ce29f7504aa6aca0
Nandhini.R <32575168+nandy20@users.noreply.github.com>
2025-03-31 19:37:24 +0530

    Create MergeTwoLL.cpp

---
+++ Cpp/Linked List/MergeTwoLL.cpp
@@ -0,0 +1,37 @@
+//https://www.geeksforgeeks.org/problems/merge-two-sorted-linked-lists/1
+Node* sortedMerge(Node* head1, Node* head2)  
+{  
+    // code here
+     Node *temp1, *temp2, *temp3,*head3;
+    temp1=head1,temp2=head2;
+    if(head1->data<head2->data){
+        head3=head1;
+       temp3=head1;
+       temp1=temp1->next;
+    }
+    else{
+        head3=head2;
+      temp3=head2;
+      temp2=head2->next;
+    }
+    while(temp1!=NULL && temp2!=NULL){
+        if(temp1->data<temp2->data){
+            temp3->next=temp1;
+            temp3=temp3->next;
+            temp1=temp1->next;
+        }
+        else{
+            temp3->next=temp2;
+            temp3=temp3->next;
+            temp2=temp2->next;
+        }
+    }
+    if(temp1!=NULL){
+        temp3->next=temp1;
+    }
+    else if(temp2!=NULL){
+        temp3->next=temp2;
+    }
+    
+    return head3;
+}  
