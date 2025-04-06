e92994b600053cef589253399ed13233106755c6
Nandhini.R <32575168+nandy20@users.noreply.github.com>
2025-04-06 20:32:31 +0530

    Create Find n-th term of series 1, 3, 6, 10, 15, 21.java

---
+++ Java/Math/Find n-th term of series 1, 3, 6, 10, 15, 21.java
@@ -0,0 +1,11 @@
+//https://www.geeksforgeeks.org/problems/find-n-th-term-of-series-1-3-6-10-15-215506/1
+class Solution {
+    static int findNthTerm(int n) {
+        // code here
+        int x = 1,sum=0;
+        for(int i=1;i<=n;i++){
+            sum = sum+ (x);
+            x++;
+        }
+        return sum;
+    }
