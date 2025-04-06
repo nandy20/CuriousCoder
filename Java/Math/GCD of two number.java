fedd4b9e02d3da73f633a2d1213f20fe63e3b9ca
Nandhini.R <32575168+nandy20@users.noreply.github.com>
2025-04-06 20:30:04 +0530

    Create GCD of two number.java

---
+++ Java/Math/GCD of two number.java
@@ -0,0 +1,17 @@
+//https://www.geeksforgeeks.org/problems/gcd-of-two-numbers3459/1
+class Solution {
+    public static int gcd(int a, int b) {
+        // code here
+        if(a ==0) return b;
+        if(b== 0) return a;
+        while(a!=b)
+        {
+            if(a>b) 
+            a = a-b;
+            if(b>a) 
+            b = b-a;
+            
+        }
+        return a;
+    }
+}
