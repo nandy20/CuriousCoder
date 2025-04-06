796328f28d2f0da9a292b2804fb390c6ce280926
Nandhini.R <32575168+nandy20@users.noreply.github.com>
2025-04-06 20:31:21 +0530

    Create Middle of Three.java

---
+++ Java/Math/Middle of Three.java
@@ -0,0 +1,15 @@
+//https://www.geeksforgeeks.org/problems/middle-of-three2926/1
+class Solution {
+    int middle(int A, int B, int C) {
+        // code here
+        if (A>B && A>C){
+            return B>C ? B:C;
+        }
+        else if (B>A && B>C){
+            return A>C ? A:C;
+        }
+        else{
+             return A>B ? A:B;
+        }
+    }
+}
