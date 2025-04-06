c8461db151e4f46ea9724ff5f2995e7d3bd1fb21
Nandhini.R <32575168+nandy20@users.noreply.github.com>
2025-04-06 21:08:35 +0530

    Create Find Kth Rotation.java

---
+++ Java/Array/Find Kth Rotation.java
@@ -0,0 +1,16 @@
+//https://www.geeksforgeeks.org/problems/rotation4723/1
+class Solution {
+    public int findKRotation(List<Integer> a) {
+        int x=-1;
+        for(int i=0;i<a.size()-1;i++){
+            if(a.get(i)>a.get(i+1)){
+                x=i;
+                break;
+            }
+        }
+        if(x!=-1){
+            return x+1;
+        }
+        return 0;
+    }
+}
