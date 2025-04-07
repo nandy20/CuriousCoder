7a26bafa31ea20015792e56ca52a9e1c3d9c06b9
Nandhini.R <32575168+nandy20@users.noreply.github.com>
2025-04-07 13:50:20 +0530

    Create Wave Array.py

---
+++ Python/Array/Wave Array.py
@@ -0,0 +1,9 @@
+//https://www.geeksforgeeks.org/problems/wave-array-1587115621/1
+from typing import List
+
+class Solution:
+    def convertToWave(self, arr: List[int]) -> None:
+        # code here
+        n = len(arr)
+        for i in range(0, n - 1, 2):
+            arr[i], arr[i + 1] = arr[i + 1], arr[i]
