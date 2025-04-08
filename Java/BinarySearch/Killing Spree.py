be9125af8d5130e84d7b9106d6d31999e0ad7fd5
Nandhini.R <32575168+nandy20@users.noreply.github.com>
2025-04-08 15:10:51 +0530

    Create Killing Spree.py

---
+++ Java/BinarySearch/Killing Spree.py
@@ -0,0 +1,23 @@
+//https://www.geeksforgeeks.org/problems/killing-spree3020/0
+class Solution
+{
+    long killinSpree(long n)
+    {
+        // Code Here
+        long i = 1, j = (long) Math.sqrt(n);
+        long ans = 0;
+        
+        while (i <= j) {
+            long mid = i + (j - i) / 2;
+            long sum = (mid * (mid + 1) * (2 * mid + 1)) / 6;
+            
+            if (sum > n) {
+                j = mid - 1;
+            } else {
+                ans = mid;
+                i = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
