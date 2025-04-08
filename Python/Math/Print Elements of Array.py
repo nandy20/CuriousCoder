fcd67361c60fd6f044125eb1cbb80be0b3bdfcb1
Nandhini.R <32575168+nandy20@users.noreply.github.com>
2025-04-08 14:23:18 +0530

    Create Print Elements of Array.py

---
+++ Python/Math/Print Elements of Array.py
@@ -0,0 +1,7 @@
+//https://www.geeksforgeeks.org/problems/print-elements-of-array4910/1
+class Solution:
+    # Just print the space separated array elements
+    def printArray(self, arr):
+        # code here
+        for num in arr:
+            print(num, end=" ")
