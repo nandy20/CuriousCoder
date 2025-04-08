e2f3131b9592a7066380316f17b42be1fd697d19
Nandhini.R <32575168+nandy20@users.noreply.github.com>
2025-04-08 13:51:56 +0530

    Create Insert in Middle of Linked List.py

---
+++ Python/LinkedList/Insert in Middle of Linked List.py
@@ -0,0 +1,21 @@
+//https://www.geeksforgeeks.org/problems/insert-in-middle-of-linked-list/1
+class Solution:
+    def insertInMiddle(self, head, x):
+        #code here
+        new_node = Node(x)
+        
+        if head is None:
+            head = new_node
+            return new_node
+            
+        
+        slow = head
+        fast = head
+        
+        while fast and fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next 
+        new_node.next = slow.next
+        slow.next = new_node
+        
+        return head
