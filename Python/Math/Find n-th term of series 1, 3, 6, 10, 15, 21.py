c1b0f588cd7207fd8e61bf7841a92b79cd5bda3f
Nandhini.R <32575168+nandy20@users.noreply.github.com>
2025-04-08 14:21:59 +0530

    Create Find n-th term of series 1, 3, 6, 10, 15, 21.py

---
+++ Python/Math/Find n-th term of series 1, 3, 6, 10, 15, 21.py
@@ -0,0 +1,5 @@
+//https://www.geeksforgeeks.org/problems/find-n-th-term-of-series-1-3-6-10-15-215506/1
+class Solution:
+    def findNthTerm(self, n):
+        # code here
+        return (N * (N+1))//2
