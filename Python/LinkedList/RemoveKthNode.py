e15cf8e4342a1c895f8f50a7ce398cc0822cc612
Nandhini.R <32575168+nandy20@users.noreply.github.com>
2025-04-08 13:28:12 +0530

    Create RemoveKthNode.py

---
+++ Python/LinkedList/RemoveKthNode.py
@@ -0,0 +1,18 @@
+//https://www.geeksforgeeks.org/problems/remove-every-kth-node/1
+class Solution:
+    def deleteK(self, head, k):
+        if k == 1:
+            return None
+        if k == 0:
+            return head
+        
+        count = 0
+        temp = head
+        while temp:
+            count += 1
+            if (count + 1) % k == 0:
+                if temp.next:
+                    temp.next = temp.next.next
+                count += 1
+            temp = temp.next
+        return head
