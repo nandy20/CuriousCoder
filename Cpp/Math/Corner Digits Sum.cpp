9e0fed93e9e74607b2fa38b206f0e4a45ebe5040
Nandhini.R <32575168+nandy20@users.noreply.github.com>
2025-04-11 12:08:03 +0530

    Update Corner Digits Sum.cpp

--- Cpp/Math/Corner Digits Sum.cpp
+++ Cpp/Math/Corner Digits Sum.cpp
@@ -1,9 +1,9 @@
 //https://www.geeksforgeeks.org/problems/corner-digits1317/1
-class Solution
-{
-    public int corner_digitSum(int n)
-    {
-        int sum = n%10, count = 0;
+class Solution{
+	public:
+   	int corner_digitSum(int n){
+   	    // Code here
+   	    int sum = n%10, count = 0;
         while(n>0){
             if(n==n%10 && count>0){
                 sum = sum+(n%10);
@@ -12,5 +12,5 @@ class Solution
             n=n/10;
         }
         return sum;
-    }
-}
+   	}    
+};
