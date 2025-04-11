ce17d9f412c421ebf558450c117535157809e032
Nandhini.R <32575168+nandy20@users.noreply.github.com>
2025-04-11 12:09:10 +0530

    Create Corner Digits Sum.java

---
+++ Java/Math/Corner Digits Sum.java
@@ -0,0 +1,16 @@
+//https://www.geeksforgeeks.org/problems/corner-digits1317/1
+class Solution
+{
+    public int corner_digitSum(int n)
+    {
+        int sum = n%10, count = 0;
+        while(n>0){
+            if(n==n%10 && count>0){
+                sum = sum+(n%10);
+            }
+            count++;
+            n=n/10;
+        }
+        return sum;
+    }
+}
