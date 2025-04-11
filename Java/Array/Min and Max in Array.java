2e61de4a260ead7fb7eee5aad9bd9a1079ab7ef9
Nandhini.R <32575168+nandy20@users.noreply.github.com>
2025-04-11 12:46:28 +0530

    Create Min and Max in Array.java

---
+++ Java/Array/Min and Max in Array.java
@@ -0,0 +1,19 @@
+//https://www.geeksforgeeks.org/problems/find-minimum-and-maximum-element-in-an-array4428/1
+class Solution {
+    public Pair<Integer, Integer> getMinMax(int[] arr) {
+        // Code Here
+        int num1 = arr[0];
+        int num2 = arr[0];
+        
+        for (int i = 0; i < arr.length; i++) {
+            if (num1 > arr[i]) {
+                num1 = arr[i];
+            }
+            if (num2 < arr[i]) {
+                num2 = arr[i];
+            }
+        }
+        
+        return new Pair<>(num1, num2);
+    }
+}
